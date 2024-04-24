77c22424e776f4270876b00b5a4444a2e72a460d
Panitou <joaco0mr4@gmail.com>
2024-04-24 12:12:36 -0500

    feat: Content y Nav modificados

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,30 +2,26 @@ import Gato from '../assets/img/gato-siames.png'
 
 const Content = () => {
     return (
-        <>
-            <div id="" className="flex flex-grow font-gabarito pl-24 pt-28 pb-36">
-                <div className="flex flex-col justify-center items-start">
-                    <div className="w-[800px]">
-                        <h1 className=" font-extrabold text-[#19240f] text-[80px] text-pretty leading-tight dark:text-[#a6c977]">
-                            Consigue los mejores
-                            apuntes universitarios
-                        </h1>
-                    </div>
-                    <div className="w-[800px]">
-                        <h4 className=" font-normal text-[#19240f] text-[40px] text-pretty leading-snug dark:text-white">
-                            Compra apuntes de diferentes personas y universidades, y vende y gana una comisión por cada venta.
-                        </h4>
-                    </div>
-                    <div className="mt-4 flex justify-center items-center bg-[#759f43] w-[200px] h-[64px] rounded-[20px] hover:shadow-lg">
-                        <button className="text-white font-semibold text-[25px] "> Ver Apuntes</button>
+        <div className=" font-gabarito px-24 pt-28 pb-36 justify-between">
+            <div className="inline-flex justify-between items-start w-full">
+                <div className='w-[800px]'>
+                    <h1 className="font-extrabold text-[#19240f] text-[80px] text-pretty leading-tight dark:text-[#a6c977]">
+                        Consigue los mejores apuntes universitarios
+                    </h1>
+                    <h4 className="font-normal text-[#19240f] text-[40px] text-pretty leading-snug dark:text-white">
+                        Compra apuntes de diferentes personas y universidades, y vende y gana una comisión por cada venta.
+                    </h4>
+                    <div className="mt-4 flex justify-center items-center bg-[#759f43] w-[200px] h-[64px] rounded-[20px] hover:shadow-lg dark:shadow-[#a6c977]">
+                        <button className="text-white font-semibold text-[25px]">Ver Apuntes</button>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center items-center pl-72">
+
+                <div className="flex flex-col justify-center items-center">
                     <img className="w-[512px] h-[512px]" src={Gato} alt="image-gato-siames" />
                 </div>
             </div>
-        </>
 
-    )
+        </div>
+    );
 }
 export default Content
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -11,97 +11,60 @@ import { useEffect, useState } from "react"
 const Nav = () => {
 
 
-    const [theme, setTheme] = useState(() => {
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            return "dark"
-        }
-        return "light"
-    })
+    const [theme, setTheme] = useState(() => (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
 
     useEffect(() => {
-        if (theme === "dark") {
-            document.querySelector("html").classList.add("dark")
-        } else {
-            document.querySelector("html").classList.remove("dark")
-        }
-    }, [theme])
+        document.querySelector("html").classList.toggle("dark", theme === "dark");
+    }, [theme]);
 
     const handleChangeTheme = () => {
-        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
-    }
-    return (
-        <>
-            <div className="text-white md:flex justify-center px-24 py-10">
-                <div className="font-gabarito w-[1533px] h-[80px] md:flex items-center justify-between">
+        setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
+    };
 
-                    <div className=' md:flex justify-center items-center'>
-                        <span className='pr-2 '> <IconBookmarksFilled width={48} height={48} color='#a6c977' /></span>
-                        <span className="text-[#19240f] font-semibold text-[30px] dark:text-[#a6c977]">
-                            <Link activeClass="active"
-                                to="/"
-                                spy={true}
-                                smooth={true}
-                                offset={-100}
-                                duration={500} >ApuntaDos</Link>
-                        </span>
-                    </div>
-                    <div className='text-[#19240f] dark:text-[#a6c977]' >
-                        <nav >
-                            <ul className="md:flex md:items-center justify-center ">
-                                <li >
-                                    <button className='' >
-                                        <Link activeClass="active"
-                                            to="/"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-100}
-                                            duration={500} className=''>
-                                            <span className="text-[20px] ">Inicio</span>
-                                        </Link>
-                                    </button>
-                                </li>
-                                <li className='pl-[33px]'>
-                                    <button>
-                                        <Link activeClass="active"
-                                            to="Nosotros"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-100}
-                                            duration={500} >
-                                            <span className="text-[20px]">Nosotros</span>
-                                        </Link>
-                                    </button>
-                                </li>
-                                <li className="pl-[33px]">
-                                    <button>
-                                        <Link activeClass="active"
-                                            to="Soporte"
-                                            spy={true}
-                                            smooth={true}
-                                            offset={-100}
-                                            duration={500} >
-                                            <span className="text-[20px]">Soporte</span>
-                                        </Link>
-                                    </button>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div >
-                <div>
-                    <a href="/LoginPage">
-                        <button className="flex justify-center items-center bg-[#759f43] w-[80px] h-[80px] rounded-[20px] ml-10 hover:shadow-lg">
-                            <IconUserFilled width={48} height={48} color='white' />
-                        </button>
-                    </a>
+    return (
+        <div className="text-white md:flex justify-center px-24 py-10">
+            <div className="font-gabarito w-[1533px] h-[80px] md:flex items-center justify-between">
+                <div className=' md:flex justify-center items-center'>
+                    <span className='pr-2'>
+                        <IconBookmarksFilled width={48} height={48} color='#a6c977' />
+                    </span>
+                    <span className="text-[#19240f] font-semibold text-[30px] dark:text-[#a6c977]">
+                        <Link activeClass="active" to="/" spy={true} smooth={true} offset={-100} duration={500}>ApuntaDos</Link>
+                    </span>
                 </div>
-                <div>
-                    <button className="flex justify-center items-center bg-[#759f43] w-[80px] h-[80px] rounded-[20px] ml-5 hover:shadow-lg" onClick={handleChangeTheme}>
-                        <IconSunFilled width={48} height={48} color='white' />
+                <nav className='text-[#19240f] dark:text-[#a6c977]'>
+                    <ul className="md:flex md:items-center justify-center ">
+                        <li>
+                            <Link activeClass="active" to="/" spy={true} smooth={true} offset={-100} duration={500}>
+                                <span className="text-[20px] ">Inicio</span>
+                            </Link>
+                        </li>
+                        <li className='pl-[33px]'>
+                            <Link activeClass="active" to="Nosotros" spy={true} smooth={true} offset={-100} duration={500}>
+                                <span className="text-[20px]">Nosotros</span>
+                            </Link>
+                        </li>
+                        <li className="pl-[33px]">
+                            <Link activeClass="active" to="Soporte" spy={true} smooth={true} offset={-100} duration={500}>
+                                <span className="text-[20px]">Soporte</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <a href="/LoginPage">
+                    <button className="flex justify-center items-center bg-[#759f43] w-[80px] h-[80px] rounded-[20px] ml-10 hover:shadow-lg dark:shadow-[#a6c977]">
+                        <IconUserFilled width={48} height={48} color='white' />
                     </button>
-                </div>
-            </div >
-        </>
-    )
+                </a>
+            </div>
+            <div>
+                <button className="flex justify-center items-center bg-[#759f43] w-[80px] h-[80px] rounded-[20px] ml-5 hover:shadow-lg dark:shadow-[#a6c977]" onClick={handleChangeTheme}>
+                    <IconSunFilled width={48} height={48} color='white' />
+                </button>
+            </div>
+        </div>
+    );
 }
 export default Nav
