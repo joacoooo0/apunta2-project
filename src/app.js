128edeaaf5e55b70247bbb1f4b47416f39d8ff25
Panitou <joaco0mr4@gmail.com>
2024-05-03 00:51:05 -0500

    ft-backend: login, register, logout y profile terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", authRoutes);
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,76 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
+  const passwordHash = await bcrypt.hash(password, 10);
+
   try {
     const newUser = new User({
       email,
-      password,
+      password: passwordHash,
       username,
     });
 
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updateAt: userSaved.updateAt,
+    });
   } catch (error) {
-    console.error();
+    res.status(500).json({ message: error.message });
   }
 };
-export const login = (req, res) => res.send("Login");
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Incorrect password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updateAt: userFound.updateAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+
+  if (!userFound) return res.status(400).json({ message: "user not found" });
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updateAt: userFound.updateAt,
+  });
+};
